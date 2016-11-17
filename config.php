5d6e42ffee8bcb398b625eb4a6080abafba22aec
erwannFace <e.duclos@fondationface.org>
2016-11-17 11:38:06 +0100

    Merging inscrits.php into index.php

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+    $serveur="localhost";
+    $login="trainer";
+    $pass="facesimplon2016";
+    $database = "hackathon";
+
+    // tableau qui contient le nombre max de place dispo par metier
+    $tabmax= array(
+        'Developpeur web' => 10,
+        'Big Data'=> 10,
+        'Designer'=>10,
+        'Referenciel metier'=>10
+     );
+
+?>
--- index.php
+++ index.php
@@ -20,6 +20,157 @@
 
 <body>
 
+
+<?php
+    include "config.php";
+    $info_message = [];
+    $error_message = [];
+
+    // tableau compteur initialisé a 0
+    $tabcompteur= array(
+        'Developpeur web' => 0,
+        'Big Data'=> 0,
+        'Designer'=> 0,
+        'Referenciel metier'=>0
+    );
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Une inscription est en cours.
+        $params = [];
+        $params['nom'] = $_POST['nom'];
+        $params['prenom'] = $_POST['prenom'];
+        $params['email'] = $_POST['email'];
+        $params['metier'] = $_POST['metier'];
+        $params['message'] = $_POST['message'];
+
+
+        try{//Connexion
+            $connexion= new PDO("mysql:host=$serveur;dbname=$database",$login,$pass);
+            $connexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            
+            $tableau_result = select($connexion);
+            $success = add($tableau_result,$connexion, $params);
+            if($success){
+                $msg = "Vous êtes la ".(count($tableau_result)+1)."e personne inscrite";
+                array_push($info_message, $msg);
+            }
+        }
+        catch (PDOException $e){
+            $msg = 'Echec de la connexion : '.$e->getMessage();
+            array_push($error_message, $msg);
+        }
+    }
+    
+    /* Fonction selectionnant l'ensemble des utilisateurs inscrits.
+        @params $connexion : La connection PDO
+    */
+    function select(PDO $connexion){
+        $requete=$connexion->prepare("
+            SELECT * FROM Inscrits");
+        $requete->execute();
+        $resultat=$requete->fetchall();     
+        return $resultat;
+    }
+
+    /* Fonction ajoutant un inscrit
+        @params $tableau_result : Un tableau issu du select()
+        @params $connexion : La connection PDO
+        @params $params : Un tableau avec la structure ['nom'=>.., 'prenom'=>.., 'email'=>.., 'metier'=>.., 'message'=>..]
+    */
+    function add($tableau_result,$connexion, $params){
+            error_log("Using add function width params : [".join(',', $params)."]");
+            $success = false;
+            //Je recupère mes variables globales pour pouvoir les utiliser dans cette fonction
+            global $tabcompteur, $tabmax, $error_message, $info_message;
+            $nom = $params['nom'];
+            $prenom = $params['prenom'];
+            $email = $params['email'];
+            $metier = $params['metier'];
+            $message = $params['message'];
+            if($nom != null && $prenom != null && $email != null && $metier != null){
+                if(isset($email)){
+                    if(preg_match("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $email)){
+                        $trouve = false;
+                        foreach($tableau_result as $row){
+                            $bd_nom = $row['nom'];
+                            $bd_prenom = $row['prenom'];
+                            $bd_mail = $row['email'];
+                            $bd_metier = $row['metier'];
+                        
+
+                            if(($bd_nom == $nom && $bd_prenom== $prenom) || $bd_mail== $email) {
+                                $trouve=true;
+                                $msg = 'Votre participation est déja enregistrée';
+                                array_push($error_message, $msg);
+                            }
+                        }
+
+                        if(!$trouve){
+                            compteur();
+
+                            if ($tabmax[$metier]>$tabcompteur[$metier]){
+                            $sql = "INSERT INTO Inscrits(nom, prenom, email, metier, message) VALUES (:nom,:prenom,:email,:metier,:message)";
+                            // Envoi des données
+                            $requete2= $connexion->prepare($sql);
+                            $params = array('nom'=>$nom, 'prenom'=>$prenom, 'email'=>$email, 'metier'=>$metier, 'message'=>$message);
+                            $requete2->execute($params);
+                            $msg = "Votre inscription a bien été effectuée";
+                            array_push($info_message, $msg);$success = true;
+                            $success = true;
+                            }
+
+                            else {
+                                $msg = "Navré, nous avons suffisament de ".$metier." inscrits";
+                                array_push($error_message, $msg);
+                            }
+
+
+                        }
+                    }
+                       
+                   
+                    else{
+                        $msg= 'L\'adresse mail ' .$email. ' n\'est pas valide.';
+                        array_push($error_message, $msg);
+                    }
+                }
+            }
+
+            else {
+                $msg = 'Vous n\'avez pas rempli tous les champs';
+                array_push($error_message, $msg);
+            }
+
+            return $success;
+    }
+
+
+    // Fonction definissant le tabcompteur.
+    function compteur() {
+        //Je recupère mes variables globales pour pouvoir les utiliser dans cette fonction
+        global $tableau_result;
+        global $tabmax;
+        global $tabcompteur;
+        // Par rapport au nombre d'inscripts sur un metier 
+        foreach ($tableau_result as $key => $row) {
+            $tabcompteur[$row['metier']]++;
+        }
+
+    }
+?>
+
+<?php
+    echo "<div class='errorMessages'>";
+    foreach ($error_message as $message) {
+       echo "-".$message."<br/>";
+    }
+    echo "</div>";
+    echo "<div class='infoMessages'>";
+    foreach ($info_message as $message) {
+       echo "-".$message."<br/>";
+    }
+    echo "</div>";
+?>
     <!--[if lt IE 8]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
@@ -66,20 +217,18 @@
     <!-- Colonne du nombre de participant et du planning -->
     <section class="col-md-5 section-present">
         <div class="col-md-12 block">
-            <h2 class="col-md-12">{ Nombre de participant attendus, 40 ? }</h2>
+
+            <h2 class="col-md-12">{ Nombre de places disponibles, <?= array_sum($tabmax)-  array_sum($tabcompteur); ?> ? }</h2>
             <div class="separator-present col-md-8 col-md-offset-1 center-block"></div>
-            <div class="col-md-10 col-md-offset-1 participant">
-                <p class="col-md-12">0 - dev back</p>
-            </div>
-            <div class="col-md-10 col-md-offset-1 participant">
-                <p class="col-md-12">0 - Big data</p>
-            </div>
-            <div class="col-md-10 col-md-offset-1 participant">
-                <p class="col-md-12">0 - Designer</p>
-            </div>
-            <div class="col-md-10 col-md-offset-1 participant">
-                <p class="col-md-12">0 - Ref métiers</p>
-            </div>
+            <?php
+                foreach ($tabcompteur as $key => $value) {
+                    echo "<div class='col-md-10 col-md-offset-1 participant'>";
+                    echo "<p class='col-md-12'>";
+                    echo $key, ":",$tabmax[$key]-$value;
+                    echo "</p>";
+                    echo "</div>";
+                }
+            ?>
         </div>
         <div class="col-md-12 block">
             <h2 class="col-md-12">{ planning ? } <small>JOURNÉE 1</small></h2>
@@ -128,7 +277,7 @@
                 <div id="contact-wrap">
                     <div id="contact-area">
 
-                        <form method="post" action="inscrits.php">
+                        <form method="post" action="index.php">
 
                             <input type="text" name="nom" id="nom" placeholder="Nom" />
 
